3295a19c214c4189e82824555e9fbaeddc4d8cf8
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-09-22 10:40:11 +0100

    readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,21 +1,12 @@
+#!/usr/bin/node
 const fs = require('fs');
 
-// Check if the user provided a file path as the first argument
-if (process.argv.length !== 3) {
-  console.error('Usage: node 0-readme.js <file_path>');
-  process.exit(1);
-}
-
-// Get the file path from the command line arguments
 const filePath = process.argv[2];
 
-// Read the content of the file in utf-8 encoding
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
-    // If an error occurred during reading, print the error object
-    console.error(err);
+    console.log(err);
   } else {
-    // Print the content of the file
     console.log(data);
   }
 });
