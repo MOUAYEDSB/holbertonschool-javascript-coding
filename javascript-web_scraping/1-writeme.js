d4b9b8f887c7aa58041e9f96e801b5d08f7a7731
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-09-26 15:03:45 +0100

    write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,22 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-// Check if the user provided both a file path and a string to write
-if (process.argv.length !== 4) {
-  console.error('Usage: node 1-writeme.js <file_path> "<string_to_write>"');
-  process.exit(1);
-}
-
-// Get the file path and string to write from the command line arguments
 const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
+const textData = process.argv[3];
 
-// Write the string to the file in utf-8 encoding
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    // If an error occurred during writing, print the error object
-    console.error(err);
-  } else {
-    console.log('File has been written successfully.');
-  }
+fs.writeFile(filePath, textData, 'utf-8', function (err) {
+  if (err) { console.log(err); }
 });
